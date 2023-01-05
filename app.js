3e74ae190f62fb0e6bc0c8a897ca4a9f83e0822f
TanishqRinjay <62020573+TanishqRinjay@users.noreply.github.com>
2023-01-06 00:44:20 +0530

    Uploaded all files
    Uploaded all ffiles for first time

---
+++ app.js
@@ -0,0 +1,35 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (
+      document.querySelector("input").value == "0" ||
+      document.querySelector("input").value == 0 ||
+      document.querySelector("input").value == "Error"
+    ) {
+      document.querySelector("input").value = " ";
+    }
+    if (e.target.innerText == "del") {
+      string = Math.floor(parseInt(string) / 10);
+
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = 0;
+      document.querySelector("input").value = 0;
+    } else if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else {
+      if (string == "0" || string == 0) {
+        string = e.target.innerHTML;
+      } else {
+        string = string + e.target.innerHTML;
+      }
+      document.querySelector("input").value = string;
+    }
+  });
+  window.onerror = () => {
+    document.querySelector("input").value == "Error";
+    string = 0;
+  };
+});
